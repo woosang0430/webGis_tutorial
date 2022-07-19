fa20bdd1eb83b826e2497e17938600f2290f8138
woosang0430 <woosang0430@e8ight.co.kr>
2022-07-19 10:06:40 +0900

    image Source layer 추가

--- main.js
+++ main.js
@@ -1,36 +1,50 @@
 import "./style.css";
 import { Map, View } from "ol";
-import TileLayer from "ol/layer/Tile";
-import LayerGroup from "ol/layer/Group";
+import { Image as ImageLayer, Tile as TileLayer } from "ol/layer.js";
+//import proj4 from 'proj4';
+import Static from "ol/source/ImageStatic";
+import ImageWMS from "ol/source/ImageWMS";
 import OSM from "ol/source/OSM";
-import Stamen from "ol/source/Stamen";
-
-const layer = new TileLayer({
-  opacity: 1,
-  visible: true,
-  extent: [0, 0, 0, 0],
-  zIndex: 0,
-  minResolution: 20,
-  maxResolution: 2000,
-  source: new OSM(),
+// import {getCenter} from 'ol/extent';
+// import {register} from 'ol/proj/proj4';
+// import {transform} from 'ol/proj';
+// proj4.defs(
+//  'EPSG:27700',
+//  '+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 ' +
+//   '+x_0=400000 +y_0=-100000 +ellps=airy ' +
+//   '+towgs84=446.448,-125.157,542.06,0.15,0.247,0.842,-20.489 ' +
+//   '+units=m +no_defs'
+// );
+// register(proj4);
+// const imageExtent = [0, 0, 700000, 1300000];
+const staticImageLayer = new ImageLayer({
+  source: new Static({
+    url:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/" +
+      "British_National_Grid.svg/2000px-British_National_Grid.svg.png",
+    projection: "EPSG:27700",
+    imageExtent: [0, 0, 700000, 1300000],
+  }),
 });
-
-const groupLayer = new LayerGroup({
-  layers: [
-    new TileLayer({
-      source: new Stamen({ layer: "watercolor" }),
-    }),
-    new TileLayer({
-      source: new Stamen({ layer: "terrain-labels" }),
-    }),
-  ],
+const geoserverImageLayer = new ImageLayer({
+  extent: [-13884991, 2870341, -7455066, 6338219],
+  source: new ImageWMS({
+    url: "https://ahocevar.com/geoserver/wms",
+    params: { LAYERS: "topp:states" },
+    ratio: 1,
+    serverType: "geoserver",
+  }),
 });
-
 const map = new Map({
   target: "map",
-  layers: [layer, groupLayer],
+  layers: [
+    new TileLayer({ source: new OSM() }),
+    staticImageLayer,
+    geoserverImageLayer,
+  ],
   view: new View({
+    //center: transform(getCenter(imageExtent), 'EPSG:27700', 'EPSG:3857'),
     center: [0, 0],
-    zoom: 2,
+    zoom: 4,
   }),
 });
