b55e7b6e2fa27127d34b29a6f42ac66f383c44bf
woosang0430 <woosang0430@e8ight.co.kr>
2022-07-19 10:50:42 +0900

    vector source example

--- main.js
+++ main.js
@@ -1,38 +1,21 @@
 import "./style.css";
 import { Map, View } from "ol";
-import LayerGroup from "ol/layer/Group";
-import TileWMS from "ol/source/TileWMS";
-import XYZ from "ol/source/XYZ";
-import TileLayer from "ol/layer/Tile";
-import OSM from "ol/source/OSM";
+import GeoJSON from "ol/format/GeoJSON.js";
+import VectorLayer from "ol/layer/Vector.js";
+import VectorSource from "ol/source/Vector.js";
 
-const groupLayer = new LayerGroup({
-  layers: [
-    new TileLayer({
-      source: new OSM(),
-    }),
-    new TileLayer({
-      extent: [-13884991, 2870341, -7455066, 6338219],
-      source: new TileWMS({
-        url: "https://ahocevar.com/geoserver/wms",
-        params: { LAYERS: "topp:states", TILED: true },
-        serverType: "geoserver",
-        transition: 0,
-      }),
-    }),
-    new TileLayer({
-      source: new XYZ({
-        url: "http://tile.stamen.com/terrain/{z}/{x}/{y}.png",
-      }),
-    }),
-  ],
+const vectorLayer = new VectorLayer({
+  source: new VectorSource({
+    url: "data/seoul_sgg.geojson",
+    format: new GeoJSON(),
+  }),
 });
 
 const map = new Map({
   target: "map",
-  layers: [groupLayer],
+  layers: [vectorLayer],
   view: new View({
-    center: [0, 0],
-    zoom: 2,
+    center: [14135193.892664503, 4512192.435216382],
+    zoom: 10,
   }),
 });
